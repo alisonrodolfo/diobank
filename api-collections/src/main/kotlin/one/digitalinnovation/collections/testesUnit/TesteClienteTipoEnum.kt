f135073d1d1589cc4e5228b472c1a4402560a7ed
Alison Barreiro <alison.rmb@gmail.com>
2022-01-23 19:15:43 -0300

    creation class teste Unit

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/testesUnit/TesteClienteTipoEnum.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.collections.testesUnit
+
+import one.digitalinnovation.collections.Cliente.ContaTipo
+
+fun main(){
+    ContaTipo.values().forEach { elemento ->
+        println("${elemento.name} - $elemento")
+    }
+
+    ContaTipo.values().forEach { println("${it.name} $it") }
+
+}
