0c61d61d81ea7655dfa39277cbef7aec4e58ef82
Alison Barreiro <alison.rmb@gmail.com>
2022-01-23 18:42:44 -0300

    Data Classa conta, para definição do banco

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/testes/Bancoteste.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.collections.testes
+
+import one.digitalinnovation.collections.Banco
+
+fun main(){
+    val dioOneBank = Banco("DioBank",1)
+
+    println(dioOneBank.name)
+    println(dioOneBank.numero)
+}
