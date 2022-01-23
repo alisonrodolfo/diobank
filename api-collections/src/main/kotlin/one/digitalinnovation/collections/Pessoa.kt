bd7dd8dc662889196aba911de80eb30c055c4070
Alison Barreiro <alison.rmb@gmail.com>
2022-01-23 18:42:06 -0300

    Classe pessoa foi criada

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -3,11 +3,18 @@ package one.digitalinnovation.collections
 class Pessoa {
     var name: String = "Nome"
     var cpf: String = "123.123.123-11"
+    private set
+
+    constructor()
+
+    fun pessoInfor() = "$name e $cpf"
 
 
-    inner class Endereco{
-        var rua: String = "Rua teste"
-    }
 
 }
 
+fun main(){
+    val name = Pessoa()
+
+    println(name.pessoInfor())
+}
