81ffae111f2b77335ce38105aa1ae861c4736fdd
Alison Barreiro <alison.rmb@gmail.com>
2022-01-23 21:28:37 -0300

    Implementation of the imprimeRelatorio

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Controll/imprimeRelatorio.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.collections.Controll
+
+import one.digitalinnovation.collections.Controller.Funcionario
+
+
+class imprimeRelatorio {
+    companion object {
+        fun imprimeRelatorio(funcionario: Funcionario) {
+            println(funcionario.toString())
+        }
+    }
+}
